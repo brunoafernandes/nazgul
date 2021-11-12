b632e55fc9c5efaa2e4ecafa2b1d7c9b276d3857
abreufechou <abreufechou@gmail.com>
2021-11-12 16:51:39 -0300

    primeiro commit

---
+++ Travel.Discover/Controllers/PlaceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel.Discover.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+
+    public class PlaceController : Controller
+    {
+        [HttpGet("health")]
+        public IActionResult health()
+        {
+            return Ok("working");
+        }
+    }
+}
